92eea99971030bd8909cf90a0622210ada12cf45
Anis Belkahla <belcalaanis@gmail.com>
2026-04-06 22:39:42 -0400

    post

--- backend/routes/jeux-routes.js
+++ backend/routes/jeux-routes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getJeux, getJeuId } from "../controllers/jeux-controller.js";
+import { getJeux, getJeuId, createJeu } from "../controllers/jeux-controller.js";
 
 const router = express.Router();
 
 router.get("/", getJeux);
 
 router.get("/:tid", getJeuId);
+
+router.post("/",createJeu)
 
 export default router;
