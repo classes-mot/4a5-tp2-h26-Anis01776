2b8ea894e938b9e0c3a1fbf6f7fb3f73b74d2078
Anis Belkahla <belcalaanis@gmail.com>
2026-04-06 23:16:19 -0400

    Ajout des controllers

---
+++ backend/controllers/jeux-controller.js
@@ -0,0 +1,48 @@
+const defaultGames = [
+  {
+    id: "1",
+    nom: "Catan",
+    categorie: "Strategie",
+    joueurs: "3-4",
+    duree: "60",
+  },
+  {
+    id: "2",
+    nom: "Pandemic",
+    categorie: "Cooperatif",
+    joueurs: "2-4",
+    duree: "45",
+  },
+  {
+    id: "3",
+    nom: "7 Wonders",
+    categorie: "Famille",
+    joueurs: "2-7",
+    duree: "30",
+  },
+  {
+    id: "4",
+    nom: "Echec",
+    categorie: "Strategie",
+    joueurs: "2",
+    duree: "20",
+  },
+];
+
+const getJeux = (req, res, next) => {
+  res.json({ jeux: defaultGames });
+};
+
+const getJeuId = (req, res, next) => {
+  const jeuId = req.params.tid;
+  const jeu = defaultGames.find((j) => {
+    return j.id === jeuId;
+  });
+  if (!jeu) {
+    const erreur = new Error("Jeu non trouvée");
+    erreur.code = 404;
+    return next(erreur);
+  }
+  res.json({ jeu });
+};
+export { getJeux ,getJeuId };
