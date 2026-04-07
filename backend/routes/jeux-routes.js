0b51e8e5183149323d59ec165bce137c9dcfeb29
Anis Belkahla <belcalaanis@gmail.com>
2026-04-06 23:16:44 -0400

    modification de routes jeux (separation entre routes et controllers)

--- backend/routes/jeux-routes.js
+++ backend/routes/jeux-routes.js
@@ -1,57 +1,10 @@
 import express from "express";
+import { getJeux, getJeuId } from "../controllers/jeux-controller.js";
+
 const router = express.Router();
 
-const defaultGames = [
-  {
-    id: "1",
-    nom: "Catan",
-    categorie: "Strategie",
-    joueurs: "3-4",
-    duree: "60",
-  },
-  {
-    id: "2",
-    nom: "Pandemic",
-    categorie: "Cooperatif",
-    joueurs: "2-4",
-    duree: "45",
-  },
-  {
-    id: "3",
-    nom: "7 Wonders",
-    categorie: "Famille",
-    joueurs: "2-7",
-    duree: "30",
-  },
-  {
-    id: "4",
-    nom: "Echec",
-    categorie: "Strategie",
-    joueurs: "2",
-    duree: "20",
-  },
-];
+router.get("/", getJeux);
 
-router.get("/", (req, res, next) => {
-  console.log("Get Request  in Jeux");
-  let jeu = null;
-  if (defaultGames.length !== 0) {
-    jeu = defaultGames;
-  }
-  res.json({ jeu });
-});
-
-router.get("/:tid", (req, res, next) => {
-  const jeuId = req.params.tid;
-  const jeu = defaultGames.find((j) => {
-    return j.id === jeuId;
-  });
-  if (!jeu) {
-    const erreur = new Error("Jeu non trouvée");
-    erreur.code = 404;
-    return next(erreur);
-  }
-  res.json({ jeu });
-});
+router.get("/:tid", getJeuId);
 
 export default router;
