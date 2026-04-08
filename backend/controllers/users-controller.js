aa353162225528a61e5c48ca8043a72d002153a5
Anis Belkahla <belcalaanis@gmail.com>
2026-04-07 20:44:21 -0400

    Suppresion de la variable local(defaultUser) + supression de la generation d'un token lorsque on cree un compte(seulement lorsque on se connecte maintenant) + implementation de la base de donnee pour la methode de connexion (sign in) ps:trop de commentaire pour peur de code :(

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -3,15 +3,6 @@ import jwt from "jsonwebtoken";
 import HttpError from "../util/Http-error.js";
 import { User } from "../models/User.js";
 
-let defaultUsers = [
-  {
-    id: "u1",
-    name: "Test Testeur",
-    email: "test@test.test",
-    password: "test123",
-  },
-];
-
 const registerUser = async (req, res, next) => {
   const { email, password } = req.body;
   let existingUser;
@@ -37,56 +28,40 @@ const registerUser = async (req, res, next) => {
       new HttpError("Enregistrement echouer recommence plus tard", 500),
     );
   }
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+};
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (erreur) {
+    return next(new HttpError("Echec de connexion1", 500));
+  }
+  if (!existingUser || existingUser.password !== password) {
+    return next(
+      new HttpError("Identification echouer,Verifier les identifions", 401),
+    );
+  }
+
   let token;
   try {
     token = jwt.sign(
-      { userId: createdUser.id, email: createdUser.email },
+      { userId: existingUser.id, email: existingUser.email },
       "cleTresTresTresSecret???",
       { expiresIn: "1h" },
     );
   } catch (err) {
-    const error = new HttpError(
-      "Signing up failed, please try again later.",
-      500,
-    );
+    const error = new HttpError("Erreur lors de la generation de la cle", 500);
     return next(error);
   }
-  res
-    .status(201)
-    .json({ user: createdUser.toObject({ getters: true }), token: token });
-};
-
-const login = (req, res, next) => {
-  const { email, password } = req.body;
-  const identifiedUser = defaultUsers.find(
-    (u) => (u.email === email) & (u.password === password),
-  );
-  if (!identifiedUser) {
-    res
-      .status(401)
-      .json({ message: "Identification echouee , verifier les identifiants" });
-  } else {
-    let token;
-    try {
-      token = jwt.sign(
-        { userId: identifiedUser.id, email: identifiedUser.email },
-        "cleTresTresTresSecret???",
-        { expiresIn: "1h" },
-      );
-    } catch (err) {
-      const error = new HttpError(
-        "Signing up failed, please try again later.",
-        500,
-      );
-      return next(error);
-    }
-    res.json({
-      message: "Identification reussie",
-      userId: identifiedUser.id,
-      email: identifiedUser.email,
-      token: token,
-    });
-  }
+  res.status(200).json({
+    userId: existingUser.id,
+    email: existingUser.email,
+    token: token,
+  });
 };
 
 export { registerUser, login };
